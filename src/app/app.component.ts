7528a34c43f9102e1e0d36aea9b0e49db0b2e773
Eric Esquibel <eric@abmp.com>
2019-04-11 15:42:05 -0600

    Added some comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray } from '@angular/forms';
-import { QuestionModel } from './model/question.model';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormArray } from '@angular/forms';
+
 import { Answer } from './model/answer.model';
+import { QuestionModel } from './model/question.model';
 
 interface AnswerForm {
   Answer: Answer;
@@ -16,21 +17,36 @@ interface AnswerForm {
 export class AppComponent implements OnInit {
   title = 'reactive-form-example';
 
+  /**
+   * Form Array for storing the answers
+   */
   public arrayOfAnswers: FormArray;
 
+  /**
+   * Array of questions
+   */
   public Questions: QuestionModel[];
 
+  /**
+   * Computed score
+   */
   public Score: number;
 
   constructor(private http: HttpClient) {
   }
 
   public ngOnInit(): void {
+    // Create an FormArray to store an array of FormGroups that the QuestionComponent will add to
+    // Could also just be an array of FormControls
     this.arrayOfAnswers = new FormArray([]);
 
+    // I stored my question data as a JSON file, so we need to fetch it
     this.http.get<QuestionModel[]>('assets/data/questions.json').subscribe(result => this.Questions = result );
   }
 
+  /**
+   * Computes the score when the form is submitted
+   */
   public Save(): void {
     if (this.arrayOfAnswers.valid) {
       const values: AnswerForm[] = this.arrayOfAnswers.value;
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -9,30 +9,54 @@ import { QuestionModel } from '../model/question.model';
 })
 export class QuestionComponent implements OnInit {
 
+  /**
+   * The Form Array from the parent component
+   */
   @Input()
   public parent: FormArray;
 
+  /**
+   * The question to display
+   */
   @Input()
   public Data: QuestionModel;
 
+  /**
+   * The form group for this question, hosts the form controls
+   */
   public form: FormGroup;
 
+  /**
+   * Quick reference to the radio button control
+   */
   public AnswerRadio: FormControl;
 
   constructor() { }
 
   public ngOnInit(): void {
 
+    // Default value for the radio button
     const defaultValue = null; // this.Data.Answers[2]; // Selects the 3rd (zero index) answer to set as default
 
     console.log('defaultValue', defaultValue);
 
+    // Instantiate a form control that represents our radio button
+    // Validators.required add a form validator that makes the control required
     this.AnswerRadio = new FormControl(defaultValue, [Validators.required]);
 
+    //  Other validators:
+    //    Validators.min(5) // Numberic value must be a miniumum of 5
+    //    Validators.minLength(10) // String length must be a minimum of 10
+    //    Validators.maxLength(255) // String length must be a maximum of 255
+    //    Validators.pattern(/[\w\d\s]+/) // String must match this regular expression
+
+    // Create a form group for this component and add the created form control to it
     this.form = new FormGroup({
+      // {Control Name}: {FormControl instance}
       Answer: this.AnswerRadio
     });
 
+    // Add this form group to the Form Array from the parent component
     this.parent.push(this.form);
   }
 }
